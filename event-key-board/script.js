8bc54eb475d22e824cc0c9f0ec886f01e1d785de
Layla Ventillari <laylaventilari5@gmail.com>
2022-09-04 23:15:47 -0300

    changes

--- event-key-board/script.js
+++ event-key-board/script.js
@@ -2,63 +2,63 @@
 document.onkeyup = keyEvent;
 function keyEvent(e) {
   if (e.keyCode == 38 ){
-    document.getElementById('release').innerHTML = '(Key up) Up Arrow ';
+    document.getElementById('release').innerHTML = 'Up Arrow - Key up';
   }
   else if (e.keyCode == 40){
-    document.getElementById('release').innerHTML = '(Key up) Down Arrow';
+    document.getElementById('release').innerHTML = 'Down Arrow - Key up';
   }  
   else if (e.keyCode == 37){
-    document.getElementById('release').innerHTML = '(Key up) Left Arrow';
+    document.getElementById('release').innerHTML = 'Left Arrow - Key up' ;
   }  
   else if (e.keyCode == 39){
-    document.getElementById('release').innerHTML = '(Key up) Right Arrow';
+    document.getElementById('release').innerHTML = 'Right Arrow - Key up';
   }  
   else if (e.keyCode == 32){
-    document.getElementById('release').innerHTML = '(Key up) Space';
+    document.getElementById('release').innerHTML = 'Space - Key up';
   }  
   else if (e.keyCode == 13){
-    document.getElementById('release').innerHTML = '(Key up) Enter';
+    document.getElementById('release').innerHTML = 'Enter - Key up';
   }  
   else if (e.keyCode == 8){
-    document.getElementById('release').innerHTML = '(Key up) Backspace';
+    document.getElementById('release').innerHTML = 'Backspace - Key up';
   }  
   else if (e.keyCode == 16){
-    document.getElementById('release').innerHTML = '(Key up) Shift';
+    document.getElementById('release').innerHTML = 'Shift - Key up';
   }  
   else if (e.keyCode == 17){
-    document.getElementById('release').innerHTML = '(Key up) CTRL';
+    document.getElementById('release').innerHTML = 'CTRL - Key up';
   }  
 };
 
 document.onkeydown = teclado;
 function teclado(e) {
   if (e.keyCode == 38 ){
-    document.getElementById('info').innerHTML = '(Key down) Up Arrow';
+    document.getElementById('info').innerHTML = ' Up Arrow - Key down';
     
   }
   else if (e.keyCode == 40){
-    document.getElementById('info').innerHTML = '(Key down) Down Arrow';
+    document.getElementById('info').innerHTML = 'Down Arrow - Key down ';
   }  
   else if (e.keyCode == 37){
-    document.getElementById('info').innerHTML = '(Key down) Left Arrow';
+    document.getElementById('info').innerHTML = 'Left Arrow - Key down ';
   }  
   else if (e.keyCode == 39){
-    document.getElementById('info').innerHTML = '(Key down) Right Arrow';
+    document.getElementById('info').innerHTML = 'Right Arrow - Key down ';
   }  
   else if (e.keyCode == 32){
-    document.getElementById('info').innerHTML = '(Key down) Space';
+    document.getElementById('info').innerHTML = 'Space - Key down ';
   }  
   else if (e.keyCode == 13){
-    document.getElementById('info').innerHTML = '(Key down) Enter';
+    document.getElementById('info').innerHTML = 'Enter - Key down ';
   }  
   else if (e.keyCode == 8){
-    document.getElementById('info').innerHTML = '(Key down) Backspace';
+    document.getElementById('info').innerHTML = 'Backspace - Key down ';
   }  
   else if (e.keyCode == 16){
-    document.getElementById('info').innerHTML = '(Key down) Shift';
+    document.getElementById('info').innerHTML = 'Shift - Key down ';
   }  
   else if (e.keyCode == 17){
-    document.getElementById('info').innerHTML = '(Key down)CTRL';
+    document.getElementById('info').innerHTML = '(CTRL - Key down ';
   }  
   
 };
