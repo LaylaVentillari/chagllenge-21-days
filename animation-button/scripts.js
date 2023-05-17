03cd5d7e3fcbf300a82532b17365ad03a54acd63
layla-ventilari <laylaventilari5@gmail.com>
2023-05-17 13:42:37 -0400

    styles: update theme

--- animation-button/scripts.js
+++ animation-button/scripts.js
@@ -1,33 +1,37 @@
 function changeMode() {
     changeClasses();
     changeText();
-}
-
-function changeClasses(){
+  }
+  
+  function changeClasses() {
     button.classList.toggle(darkModeClass);
     h1.classList.toggle(darkModeClass);
     body.classList.toggle(darkModeClass);
     footer.classList.toggle(darkModeClass);
-
-}
-function changeText(){
+  }
+  
+  function changeText() {
     const lightMode = "Light";
     const darkMode = "Dark";
-
-    if(body.classList.contains(darkModeClass)){
-        button.innerHTML = lightMode;
-        h1.innerHTML = darkMode + " ON";
-        return;
+  
+    if (body.classList.contains(darkModeClass)) {
+      button.innerHTML = lightMode;
+      h1.innerHTML = darkMode + " ON";
+    } else {
+      button.innerHTML = darkMode;
+      h1.innerHTML = lightMode + " ON";
     }
-        button.innerHTML = darkMode;
-        h1.innerHTML = lightMode + " ON";
-}
-
-const darkModeClass ='dark-mode';
-const button = document.getElementById('mode-selector');
-const h1 = document.getElementById('page-title');
-const body = document.getElementsByTagName('body')[0];
-const footer = document.getElementsByTagName('footer')[0];
-
-
-button.addEventListener('click',changeMode);
+  }
+  
+  const darkModeClass = 'dark-mode';
+  const button = document.getElementById('mode-selector');
+  const h1 = document.getElementById('page-title');
+  const body = document.getElementsByTagName('body')[0];
+  const footer = document.getElementsByTagName('footer')[0];
+  
+  button.addEventListener('click', function() {
+    button.classList.toggle('large');
+  });
+  
+  button.addEventListener('click', changeMode);
+  
